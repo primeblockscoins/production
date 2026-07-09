012c219a952a88c1a10b8ed12f320ab90c9330da
BoX@2026 <primeblockscoin@gmail.com>
2026-07-09 13:52:42 +0530

    lock the first cut

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
 import Services from './components/Services';
-import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
@@ -14,7 +13,6 @@ function App() {
         <Hero />
         <About />
         <Services />
-        <Testimonials />
         <Contact />
       </main>
       <Footer />
